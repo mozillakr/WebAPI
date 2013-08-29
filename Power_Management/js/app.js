1696ed7e9f276f153b5d30d470c969d82d4e13f9
강경희 <st1503@daumcorp.com>
2013-08-29 17:32:30 +0900

    Power Management

--- Power_Management/js/app.js
+++ Power_Management/js/app.js
@@ -1,40 +1,66 @@
-var app = function(){
-  
+var app = function() {
+  var power = window.navigator.mozPower;
+  var lock;
+  var screenTimeout;
   return {
     /**
      * common
      */
-    $: function(id){
-      if(!id){
+    $ : function(id) {
+      if (!id) {
         return null;
       }
       return document.getElementById(id);
     },
-    addEvent: function(id, event, callBack){
+    addEvent : function(id, event, callBack) {
       var obj = $(id);
       if (obj.addEventListener) {
         obj.addEventListener(event, callBack, false);
       }
-      else{
-        obj.attachEvent('on'+event, callBack, false);
+      else {
+        obj.attachEvent('on' + event, callBack, false);
       }
     },
-    changeBrightness: function(){
-      var power = window.navigator.mozPower;
+    changeBrightness : function() {
+
       var isTheScreenEnabled = power.screenEnabled
       if (isTheScreenEnabled) {
         power.screenBrightness = $('changeValue').value;
       }
-      else{
+      else {
         power.screenEnabled = true;
         power.screenBrightness = $('changeValue').value;
       }
+    },
+    reboot : function() {
+      power.reboot();
+    },
+    powerOff : function() {
+      power.powerOff();
+    },
+    wakeLock : function() {
+      lock = window.navigator.requestWakeLock('screen');
+    },
+    unlock : function() {
+      lock.unlock();
     }
-  } //-- return
+  } // -- return
 }();
 
 window.$ = app.$;
 
-window.onload = function(){
+window.onload = function() {
   app.addEvent('changeBrightness', 'click', app.changeBrightness);
+  app.addEvent('reboot', 'click', app.reboot);
+  app.addEvent('powerOff', 'click', app.powerOff);
+  app.addEvent('wakeLock', 'click', app.wakeLock);
+  app.addEvent('unlock', 'click', app.unlock);
+
+  navigator.mozPower.addWakeLockListener(function(name, stat) {
+    var message = "The lock state for the resource '" + name + "' is: <b>"
+        + stat + '</b>';
+    $('message').innerHTML = message;
+    console.log(message);
+  });
+
 }
